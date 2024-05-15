500be151f0a4542b46eeda826b445f37a07097c0
Eddy-Morales <168015027+Eddy-Morales@users.noreply.github.com>
2024-05-15 12:02:26 -0500

    Add files via upload

---
+++ repaso2.py
@@ -0,0 +1,23 @@
+print("instragram")
+user= input("ingrese usuario: ")
+password = input("ingrese contraseña: ")
+
+if user == "eddy2000" and password== "1234":
+    print(f"welcome user {user}")
+    print("loading your posts")
+else:
+    print("sorry yor password was incorrect. please double-check your password")
+    print("sing up to see photos and videos from your fiends")
+    input("log in with facebook")
+    facebook = input("yes or no ?")
+    if facebook=="si":
+        print("welcome")
+    
+    else: 
+        mobile= input("mobile number or email")
+        name= input("full name: ")
+        username= input("username: ")
+        password= input("password: ")
+
+
+    
